4e86d83c492782869649b68228ae6d7faa0ce7b6
Karel Vervaeke <karel.vervaeke@conundra.eu>
2021-12-05 22:02:07 +0100

    Day 5 in kotlin

---
+++ src/main/java/info/vervaeke/Day5.kt
@@ -0,0 +1,103 @@
+package info.vervaeke
+
+import java.io.File
+import java.lang.Math.abs
+import kotlin.math.max
+import kotlin.math.sign
+import kotlin.text.Charsets.UTF_8
+
+class Day5(val fileName: String) {
+
+    fun part1(): Int {
+        val (segments, world) = readFile()
+
+        segments.forEach { segment ->
+            segment.orthogonalCells.forEach { cell ->
+                world.increment(cell)
+            }
+        }
+
+        return world.countCrossings()
+    }
+
+    fun part2(): Int {
+        val (segments, world) = readFile()
+
+        segments.forEach { segment ->
+            segment.allCells.forEach { cell ->
+                world.increment(cell)
+            }
+        }
+
+        return world.countCrossings()
+    }
+
+    fun readFile(): Day5Input {
+        val segments = File(fileName)
+            .readLines(UTF_8)
+            .map {
+                it.split(" -> ")
+            }
+            .map {
+                Segment(Coordinate(it[0].split(",").map { it.toInt() }), Coordinate(it[1].split(",").map { it.toInt() }))
+            }
+        val maxRow = segments.maxOf { it.from.row }
+        val maxCol = segments.maxOf { it.from.col }
+
+        return Day5Input(segments, World(
+            (0..maxRow).map {
+                (0..maxCol).map { 0 }.toMutableList()
+            }
+        ))
+    }
+
+    class World(val rows: List<MutableList<Int>>) {
+        fun increment(cell: Coordinate) {
+            rows[cell.row][cell.col] += 1
+        }
+
+        fun countCrossings() = rows.flatMap { it }
+            .count { it > 1 }
+    }
+
+    data class Coordinate(private val rowCol: List<Int>) {
+        val row = rowCol[0]
+        val col = rowCol[1]
+    }
+
+    data class Segment(val from: Coordinate, val to: Coordinate) {
+        val orthogonalCells: List<Coordinate>
+            get() =
+                if (isHorizontal() || isVertical()) {
+                    allCells
+                } else {
+                    emptyList()
+                }
+
+
+        val allCells: List<Coordinate>
+            get() {
+                val dx = (to.col - from.col).sign
+                val dy = (to.row - from.row).sign
+                return (0 until length).map {
+                    Coordinate(listOf(from.row + it * dy, from.col + it * dx))
+                }
+            }
+
+        val length: Int
+            get() {
+                return max(abs(from.row - to.row), abs(from.col - to.col)) + 1
+            }
+
+        fun isHorizontal() = from.row == to.row
+        fun isVertical() = from.col == to.col
+    }
+
+    data class Day5Input(val segments: List<Segment>, val world: World)
+}
+
+fun main() {
+    println(Day5(DAY5_INPUT).part1())
+    println(Day5(DAY5_INPUT).part2())
+}
+
---
+++ src/test/java/info/vervaeke/Day5Test.kt
@@ -0,0 +1,32 @@
+package info.vervaeke
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class Day5Test {
+
+    @Test
+    fun `segment`() {
+        val p1 = Day5.Coordinate(listOf(2, 0))
+        val p2 = Day5.Coordinate(listOf(2, 1))
+        val p3 = Day5.Coordinate(listOf(2, 2))
+        val p4 = Day5.Coordinate(listOf(2, 3))
+        val p5 = Day5.Coordinate(listOf(2, 4))
+        val p6 = Day5.Coordinate(listOf(2, 5))
+
+        assertThat(Day5.Segment(p1, p6).orthogonalCells)
+            .isEqualTo(listOf(p1, p2, p3, p4, p5, p6))
+    }
+
+    @Test
+    fun `day5 part 1`() {
+        assertThat(Day5(DAY5_SAMPLE).part1()).isEqualTo(5)
+    }
+
+    @Test
+    fun `day5 part 2`() {
+        assertThat(Day5(DAY5_SAMPLE).part2()).isEqualTo(12)
+    }
+
+}

49674c6ade5f5e06fc79b09b3a92039d1e9b795b
Karel Vervaeke <karel.vervaeke@conundra.eu>
2021-12-06 08:36:15 +0100

    Day 6, with broken parts, but will fix in next commit

--- src/main/java/info/vervaeke/Day6.kt
+++ src/main/java/info/vervaeke/Day6.kt
@@ -5,22 +5,44 @@ import kotlin.text.Charsets.UTF_8
 
 class Day6(val fileName: String) {
 
-    fun part1(): Int {
-        return 0
+    fun part1(): Long {
+        var fish = readFile().lines
+        var counts = fish.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+
+        (0..255).forEach {
+            counts = iteration2(counts).toMap()
+        }
+        return counts.values.sum()
     }
 
-    fun part2(): Int {
+    fun iteration2(values: Map<Long, Long>) : Map<Long, Long> {
+        val result = mutableMapOf<Long, Long>()
+        result.put(0, values.get(1)?:0)
+        result.put(1, values.get(2)?:0)
+        result.put(2, values.get(3)?:0)
+        result.put(3, values.get(4)?:0)
+        result.put(4, values.get(5)?:0)
+        result.put(5, values.get(6)?:0)
+        result.put(6, (values.get(7)?:0 )+ (values.get(0)?:0))
+        result.put(7, values.get(8)?:0)
+        result.put(8, values.get(0)?:0)
+
+        return result
+    }
+
+    fun part2(): Long {
         return 0
     }
 
     fun readFile(): Day6Input {
         val lines = File(fileName)
             .readLines(UTF_8)
+            .get(0)
 
-        return Day6Input(lines)
+        return Day6Input(lines.split(",").map{ it.toLong()})
     }
 
-    data class Day6Input(val lines: List<String>)
+    data class Day6Input(val lines: List<Long>)
 }
 
 fun main() {
--- src/test/java/info/vervaeke/Day6Test.kt
+++ src/test/java/info/vervaeke/Day6Test.kt
@@ -8,7 +8,12 @@ class Day6Test {
 
     @Test
     fun `day6 part 1`() {
-        assertThat(Day6(DAY6_SAMPLE).part1()).isEqualTo(42)
+        assertThat(Day6(DAY6_SAMPLE).part1()).isEqualTo(5934)
+    }
+    @Test
+    fun `day6 iterate`() {
+//        assertThat(Day6(DAY6_SAMPLE).iteration2(listOf(3,4,3,1,2))).isEqualTo(listOf(2,3,2,0,1))
+//        assertThat(Day6(DAY6_SAMPLE).iteration2(listOf(2,3,2,0,1))).isEqualTo(listOf(1,2,1,6,0,8))
     }
 
     @Test

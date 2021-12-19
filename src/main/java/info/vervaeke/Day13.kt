30f7b424c81df5527d24c1020cea8f96ccedd335
Karel Vervaeke <karel.vervaeke@conundra.eu>
2021-12-19 15:04:32 +0100

    Day13 part 1

---
+++ src/main/java/info/vervaeke/Day13.kt
@@ -0,0 +1,154 @@
+package info.vervaeke
+
+class Day13() {
+
+    companion object {
+        fun parse(lines: List<String>): Pair<Paper, List<FoldInstruction>> {
+            val points = lines.takeWhile { !it.isEmpty() }
+                .map {
+                    val values = it.split(",").map { it.toInt() }
+                    Coordinate(values[0], values[1])
+                }
+            val instructions = lines.drop(points.size + 1)
+                .map {
+                    "fold along (.)=(\\d*)$".toRegex().find(it)!!
+                }
+                .filterNotNull()
+                .map { it.groups }
+                .map {
+                    it.get(1)!!.value to it.get(2)!!.value.toInt()
+                }
+                .map {
+                    val direction = if (it.first == "y") {
+                        Direction.HORIZONTAL
+                    } else {
+                        Direction.VERTICAL
+                    }
+                    FoldInstruction(direction, it.second)
+                }
+            return Paper(points.toSet()) to instructions
+        }
+
+        fun part1(input: Pair<Paper, List<FoldInstruction>>): Int {
+            val originalPaper = input.first
+            val instructions = input.second
+
+            return originalPaper.fold(instructions.first()).points.size
+        }
+
+        fun part2(input: Pair<Paper, List<FoldInstruction>>): Int {
+            val originalPaper = input.first
+            val instructions = input.second
+
+            val result = instructions.fold(originalPaper) { paper, instr -> paper.fold(instr) }
+
+            println(result.pretty())
+            return result
+                .points.size
+
+        }
+    }
+
+    data class Paper(val points: Set<Coordinate>) {
+
+        fun pretty(): String {
+            val grid = (0..points.maxOf { it.y }).map { y ->
+                (0..points.maxOf { it.x }).map { x ->
+                    Coordinate(x, y) in points
+                }
+            }
+
+            return grid.joinToString("\n") { row ->
+                row.joinToString("") {
+                    if (it) {
+                        "#"
+                    } else {
+                        "."
+                    }
+                }
+            }
+        }
+
+        fun fold(foldInstruction: FoldInstruction): Paper {
+            if (foldInstruction.direction == Direction.HORIZONTAL) {
+                return foldHorizontal(foldInstruction.line)
+            } else {
+                return foldVertical(foldInstruction.line)
+            }
+        }
+
+        fun foldHorizontal(line: Int): Paper {
+            println("Fold horizontal on line $line")
+            val maxY = bounds().second.y
+
+            assert(points.all { it.y != line })
+
+            val newPoints = points.map {
+                if (it.y < line) {
+                    it
+                } else {
+                    Coordinate(it.x, line - (it.y - line))
+                }
+            }
+
+//            val newTop = line - (maxY - line)
+//            if (newTop < 0) {
+//                return Paper(newPoints.map { Coordinate(it.x, it.y - newTop) }.toSet())
+//            } else {
+
+            println("old bounds: ${bounds()}")
+            val result = Paper(newPoints.toSet())
+
+            println("new bounds: ${result.bounds()}")
+            assert(newPoints.all { it.y >= 0})
+            return result
+//            }
+        }
+
+        fun bounds(): Pair<Coordinate, Coordinate> {
+            val minX = points.minOf { it.x}
+            val maxX = points.maxOf { it.x}
+            val minY = points.minOf {it.y}
+            val maxY = points.maxOf {it.y}
+
+            return Coordinate(minX, minY) to Coordinate(maxX, maxY)
+        }
+
+        fun foldVertical(line: Int): Paper {
+            val maxX = points.maxOf { it.x }
+
+            val newPoints = points.map {
+                if (it.x < line) {
+                    it
+                } else {
+                    Coordinate(line - (it.x - line), it.y)
+                }
+            }
+
+            val newLeft = line - (maxX - line)
+            if (newLeft < 0) {
+                assert(false)
+                return Paper(newPoints.map { Coordinate(it.x - newLeft, it.y) }.toSet())
+            } else {
+                return Paper(newPoints.toSet())
+            }
+        }
+    }
+
+    data class Coordinate(val x: Int, val y: Int)
+
+    enum class Direction {
+        HORIZONTAL,
+        VERTICAL
+    }
+
+    data class FoldInstruction(val direction: Direction, val line: Int)
+
+}
+
+
+fun main() {
+    println(Day11().part1(Day11.parse(inputLinesOfDay(11))))
+//    println(Day11(inputLinesOfDay(11)).part2())
+}
+
---
+++ src/test/java/info/vervaeke/Day13Test.kt
@@ -0,0 +1,197 @@
+package info.vervaeke
+
+import info.vervaeke.Day13.Coordinate
+import info.vervaeke.Day13.Direction.HORIZONTAL
+import info.vervaeke.Day13.Direction.VERTICAL
+import info.vervaeke.Day13.FoldInstruction
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class Day13Test {
+
+    val SAMPLE1 = """
+            6,10
+            0,14
+            9,10
+            0,3
+            10,4
+            4,11
+            6,0
+            6,12
+            4,1
+            0,13
+            10,12
+            3,4
+            3,0
+            8,4
+            1,10
+            2,14
+            8,10
+            9,0
+            
+            fold along y=7
+            fold along x=5""".trimIndent().lines()
+
+    @Test
+    fun `parse`() {
+        assertThat(Day13.parse(SAMPLE1))
+            .isEqualTo(
+                Day13.Paper(
+                    setOf(
+                        Coordinate(6, 10),
+                        Coordinate(0, 14),
+                        Coordinate(9, 10),
+                        Coordinate(0, 3),
+                        Coordinate(10, 4),
+                        Coordinate(4, 11),
+                        Coordinate(6, 0),
+                        Coordinate(6, 12),
+                        Coordinate(4, 1),
+                        Coordinate(0, 13),
+                        Coordinate(10, 12),
+                        Coordinate(3, 4),
+                        Coordinate(3, 0),
+                        Coordinate(8, 4),
+                        Coordinate(1, 10),
+                        Coordinate(2, 14),
+                        Coordinate(8, 10),
+                        Coordinate(9, 0),
+                    )
+                ) to listOf(FoldInstruction(HORIZONTAL, 7), FoldInstruction(VERTICAL, 5))
+            )
+    }
+
+    @Test
+    fun `pretty`() {
+        val actual = Day13.parse(SAMPLE1).first.pretty()
+
+        assertThat(
+            actual
+        )
+            .isEqualTo(
+                """
+                    ...#..#..#.
+                    ....#......
+                    ...........
+                    #..........
+                    ...#....#.#
+                    ...........
+                    ...........
+                    ...........
+                    ...........
+                    ...........
+                    .#....#.##.
+                    ....#......
+                    ......#...#
+                    #..........
+                    #.#........
+                """.trimIndent()
+            )
+    }
+
+    @Test
+    fun `foldHorizontal`() {
+        val grid = Day13.parse(SAMPLE1).first.fold(FoldInstruction(HORIZONTAL, 7)).pretty()
+        assertThat(grid)
+            .isEqualTo(
+                """
+    #.##..#..#.
+    #...#......
+    ......#...#
+    #...#......
+    .#.#..#.###
+""".trimIndent()
+            )
+    }
+
+    @Test
+    fun `foldVertical`() {
+        val grid = Day13.parse(SAMPLE1).first
+            .fold(FoldInstruction(HORIZONTAL, 7))
+            .fold(FoldInstruction(VERTICAL, 5))
+            .pretty()
+        assertThat(grid)
+            .isEqualTo(
+                """
+#####
+#...#
+#...#
+#...#
+#####""".trimIndent()
+            )
+    }
+
+    @Test
+    fun `count dots`() {
+        val folded = Day13.parse(SAMPLE1).first.fold(FoldInstruction(HORIZONTAL, 7))
+        assertThat(folded.points.size)
+            .isEqualTo(17)
+    }
+
+    @Test
+    fun `part1 sample`() {
+        assertThat(Day13.part1(Day13.parse(SAMPLE1)))
+            .isEqualTo(16)
+    }
+
+    @Test
+    fun `check initial paper size`() {
+        val input = Day13.parse(inputLinesOfDay(13))
+        assertThat(input.first.points.minOf { it.x })
+            .isEqualTo(0)
+        assertThat(input.first.points.maxOf { it.x })
+            .isEqualTo(1310)
+        assertThat(input.first.points.minOf { it.y })
+            .isEqualTo(0)
+        assertThat(input.first.points.maxOf { it.y })
+            .isEqualTo(894)
+    }
+
+    @Test
+    fun `more horizontal folding`() {
+        // ....## -> ##...
+        assertThat(
+            Day13.Paper(setOf(Coordinate(0, 4), Coordinate(0, 5)))
+                .fold(FoldInstruction(HORIZONTAL, 0))
+        )
+            .isEqualTo(Day13.Paper(setOf(Coordinate(0, 0), Coordinate(0, 1))))
+
+        // .....# -> .|...# -> #...
+        assertThat(
+            Day13.Paper(setOf(Coordinate(0, 4), Coordinate(0, 5)))
+                .fold(FoldInstruction(HORIZONTAL, 1))
+        )
+            .isEqualTo(Day13.Paper(setOf(Coordinate(0, 0), Coordinate(0, 1))))
+
+        // ....## -> ..|.## -> ##
+        assertThat(
+            Day13.Paper(setOf(Coordinate(0, 4), Coordinate(0, 5)))
+                .fold(FoldInstruction(HORIZONTAL, 2))
+        )
+            .isEqualTo(Day13.Paper(setOf(Coordinate(0, 0), Coordinate(0, 1))))
+
+        // ....## -> ...|## -> .##
+        assertThat(
+            Day13.Paper(setOf(Coordinate(0, 4), Coordinate(0, 5)))
+                .fold(FoldInstruction(HORIZONTAL, 3))
+        )
+            .isEqualTo(Day13.Paper(setOf(Coordinate(0, 1), Coordinate(0, 2))))
+
+        // .#|#. -> .#
+        assertThat(
+            Day13.Paper(setOf(Coordinate(0, 1), Coordinate(0, 3)))
+                .fold(FoldInstruction(HORIZONTAL, 2))
+        )
+            .isEqualTo(Day13.Paper(setOf(Coordinate(0, 1))))
+    }
+
+    @Test
+    fun `part1`() {
+        // 1020 is too high :( -> Dammit I didn't read to only do the first fild
+        // 102 is too low :( -> Same problem
+        assertThat(Day13.part1(Day13.parse(inputLinesOfDay(13))))
+            .isEqualTo(0)
+    }
+
+}
